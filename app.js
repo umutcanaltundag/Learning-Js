e5ff4f5e0247c4af9503bb20655cb669f62e60e5
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-15 16:37:59 +0300

    Değişkenler tamamlandı

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
+/*
 //  Document write ile çıktı verme.
 // -- Komutları test etmek için kullanılır, sonuçları ekrana yazdırmak için - document.write("...") kullanılır. --
-
-//  document.writeln("Hello World <br>"); - line 
-//  document.write("Umut Can <br>");
-//  document.write("5.5");
+document.writeln("Hello World <br>"); - line 
+document.write("Umut Can <br>");
+document.write("5.5");
 
 
 //-- Tarayıcı konsoluna yazı yazdırma için, console.log("..."); metodunu kullanırız.--
 
-// console.log("Hello World");
-// console.log("Umut Can");
-// console.log(3.53);
-// console.log(true);
-// console.log({name:"Umut", surname:"Can"});
+ console.log("Hello World");
+ console.log("Umut Can");
+ console.log(3.53);
+ console.log(true);
+ console.log({name:"Umut", surname:"Can"});
 
 // // Konsolu temizler console.clear();
 
-// let a=5;
-// let b=10;
+let a=5;
+let b=10;
 
 
-// console.log("İki sayının toplamı", a+b);
-// console.clear();
+ console.log("İki sayının toplamı", a+b);
+ console.clear();
+
 
 // -- Alert (Uyarı) Popup--
+ console.log(window);
+let a= 10;
+ let b= 12;
+alert("İki sayının toplamı  " + (a+b));
+alert("F5 Tuşuna lütfen basmayınız")
 
-// console.log(window);
-// let a= 10;
-// let b= 12;
-// alert("İki sayının toplamı  " + (a+b));
-// alert("F5 Tuşuna lütfen basmayınız")
-
-// console.warn("Hata oluştu");
-// debugger;
-// console.log(location.port);
-// console.log(location.port);
-// console.log(location.port);
-// console.log(location.port);
+console.warn("Hata oluştu");
+debugger;
+console.log(location.port);
+console.log(location.port);
+console.log(location.port);
+console.log(location.port);
 
 /*-- Scope (Kapsam) --
     ->Global Scope (Heryerden erişilebilir)
     ->Function Scope 
     -> Block Scope 
-*/
 
-// var Umut =10;
 
-// console.log(Umut);
+var Umut =10;
 
-// global scope
-// var a = 5;
-// while (a > 6) {
+console.log(Umut);
 
-// }
+global scope
+var a = 5;
+while (a > 6) {
+// işlemler
+}
 
-// if (true) {
-
-// }
-// function method1(){
-//     console.log(a);
-// }
-// method1();
+ if (true) {
+ // işlemler
+}
+function method1(){
+    console.log(a);
+}
+method1();
 
 
 // function scope 
@@ -80,5 +80,62 @@ function method1() {
     }
 }
 
-// console.log(a);
+console.log(a);
 method1();
+*/
+
+// Var - Let - Const
+
+/*
+var/let/const degiskenIsmi = degiskenDegeri;
+let sayi =10;
+*/
+
+// var : function scope -- ram bellekte çok fazla yer kaplar
+// let const : block  scope özelliğine sahiptir. 
+
+
+// function selamVer() {
+//     var selam = "Herkese selam"; //function scope
+//     if (true) {
+//         let b = 10;
+//     }
+//     console.log(b);
+//     console.log(selam);
+// }
+// selamVer();
+
+// var a = 5;
+// var a = 10;
+// console.log(a);
+
+// if(true){
+// let a = 5;
+// let b = 10;
+
+// console.log(a);
+// }
+
+
+// LET & CONST arasındaki fark ?
+
+//const (constant) : sabit , değişmez
+
+// const a = 10;
+// a = 15;
+// console.log(a);
+
+
+// let b = 5;
+// b=7;
+// b=12;
+// console.log(b);
+
+const user = {
+    username :"umut",
+    password : "123"
+}
+
+user.username="can"
+
+console.log(user);
