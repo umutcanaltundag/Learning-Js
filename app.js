a550b5d026fd4b42fa108b7e7ba3dca9015a1de0
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-19 19:19:17 +0300

    Debugger ve Tooltip Tamamlandı

--- app.js
+++ app.js
@@ -794,7 +794,7 @@ switch (secim){
         break;
 }
 
-*/
+
 
 //!------------------- TÜR DÖNÜŞÜMLERİ -------------------
 
@@ -844,3 +844,21 @@ switch (secim){
 
 
 // let rakamlar = Number([1,2,3,4]);
+
+//!-----Breakpoint ve Tooltip nedir ?-----
+
+//function ortalamaBul(vize1 , vize2 , finalNot){
+    //kodları
+    //return "75;"
+}
+
+// debugger;
+// let a = 10;
+// let b = 15;
+// let c = a+b;
+// let d = a+b+c;
+// let e = a+c+b;
+
+// let sonuc = a+b+c+d+e;
+
+// console.log("Sonuç :" ,sonuc);
