d34f493ef1bd338e5a428b4ff774483b5ab013ca
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-19 18:25:16 +0300

    Tür Dönüşümü Tamamlandı

--- app.js
+++ app.js
@@ -733,7 +733,7 @@ switch (secim) {
 
 
 
-*/
+
 //!-----TYT PUAN HESAPLAMA------
 
         // ? SORULAR
@@ -793,3 +793,54 @@ switch (secim){
         alert("Lütfen geçerli aralıkta değer giriniz!")
         break;
 }
+
+*/
+
+//!------------------- TÜR DÖNÜŞÜMLERİ -------------------
+
+
+//    strings, numbers, booleans, undefined, and null. 
+//    object , function
+
+// let a = 5;
+// let b = Number("10");
+// console.log(a+b);
+
+
+// let c = Number(b);
+// console.log(typeof c);
+// console.log(a+c);
+
+
+//parseInt()
+
+
+// let a = 8;
+// let b = parseFloat("12");
+// console.log(a+b);
+// console.log(typeof b);
+
+//NUMBER TİPİNDEN STRİNG VERİ TİPİNE DÖNÜŞTÜRMEK
+
+// let x = String(55);
+// let x = (55).toString(); 
+// console.log(typeof x);                                                                                                                                                                                                           
+// console.log(x);                                                                                                                                     
+
+
+// let sonuc = String(true)
+// console.log(typeof sonuc);
+// console.log(sonuc);
+
+
+// let a = Number("B")
+// Not a Number = Bu bir sayı değil
+// console.log(a);
+
+
+// let rakamlar = String([1,2,3,4]);
+// console.log(typeof rakamlar);
+// console.log(rakamlar);
+
+
+// let rakamlar = Number([1,2,3,4]);
