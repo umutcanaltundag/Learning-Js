6ed89a96f2c441054aec260dd34802ebfa0bc5ed
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-21 08:17:54 +0300

    Break & Continue Tamamlandı

--- app.js
+++ app.js
@@ -902,7 +902,7 @@ switch (secim){
 
 // console.log("Toplam :" , toplam);
 
-*/
+
 
 
 // 1'den 10'a kadar yazdıralım
@@ -967,3 +967,35 @@ switch (secim){
 // } while (sayac<=20);
 
 // console.log("Toplam :" , toplam);
+
+*/
+
+//break - continue
+
+//1'den 10'a kadar sayıları yazdıralım 8'e geldiğimizde döngüden çıkalım
+
+// let sayac =1;
+
+// while(sayac<=10){
+//     console.log(sayac);
+//     if(sayac==8){
+//         break;
+//     }
+//     sayac++;
+// }
+
+
+//? Continue
+// 1 2 3 4 5 6 7 9 10
+
+// let sayac =0;
+// while(sayac<=10){
+//     sayac++;
+//     if(sayac>10){
+//         break;
+//     }
+//     if(sayac==8){
+//         continue;
+//     }
+//     console.log(sayac); 
+// }
