3e68f83b7837a20bd025aeb947874cecf1221ddd
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-21 15:50:36 +0300

    Asal Sayı Bulma Uygulaması

--- app.js
+++ app.js
@@ -1006,9 +1006,36 @@ switch (secim){
 
 //  Çarpım tablosu
 
-for(let i = 1; i<=10; i++){
-    for(let j =1; j<=10; j++){
-        console.log(i+"x"+j+"="+(i*j));
+// for(let i = 1; i<=10; i++){
+//     for(let j =1; j<=10; j++){
+//         console.log(i+"x"+j+"="+(i*j));
+//     }
+//     console.log("-----------------------------------");
+// }
+
+
+
+// Asal Sayı Bulma Uygulaması
+
+//    15 = 2, 3, 4, 5, 6, 7.5
+
+// let a = Math.floor(7.9);
+
+// console.log(a);
+
+//15 
+
+let sayi =Number(prompt("Lütfen bir sayı giriniz :"));  
+let sonuc = true;
+for(let i=2; i<=Math.floor(sayi/2); i++){
+    if(sayi%i==0){
+         //Asal değildir
+         sonuc=false;
+         break;
     }
-    console.log("-----------------------------------");
+}
+if(sonuc){
+    alert(sayi +" asaldır.");
+}else {
+    alert(sayi +" asal değildir!");
 }
