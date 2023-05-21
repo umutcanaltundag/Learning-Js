fa20ae68383b45cb6e72e6484feec75e0882a3de
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-21 08:40:54 +0300

    Çarpım Tablosu

--- app.js
+++ app.js
@@ -968,7 +968,8 @@ switch (secim){
 
 // console.log("Toplam :" , toplam);
 
-*/
+
+
 
 //break - continue
 
@@ -999,3 +1000,15 @@ switch (secim){
 //     }
 //     console.log(sayac); 
 // }
+
+*/
+
+
+//  Çarpım tablosu
+
+for(let i = 1; i<=10; i++){
+    for(let j =1; j<=10; j++){
+        console.log(i+"x"+j+"="+(i*j));
+    }
+    console.log("-----------------------------------");
+}
