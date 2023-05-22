65427f219175c5f4e57169ddb63ca3bf125001a2
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-22 13:29:08 +0300

    Mükemmel Sayı Uygulaması

--- app.js
+++ app.js
@@ -237,7 +237,7 @@ console.log(typeof func);
 
 //!--------ARİTMETİK OPERATÖRLER--------------
 
-/*
+
 -------->   = atama operatörü önemli !
     +
     -
@@ -1159,7 +1159,7 @@ switch (secim){
 //     // void = geriye değer döndürmeyen demektir.
 // }
 
-*/
+
 
 
 // Kelime sayısı bulma uygulaması
@@ -1183,4 +1183,33 @@ switch (secim){
 //         }
 //     }
 //     console.log("Harf sayısı : " + toplam );
+// }
+
+*/
+
+
+// Mükemmel Sayı Uygulaması
+
+// 6  - 28  - 496
+// ---> Bir sayıyı tam bölünmesi için maksimum yarısına kadar yolumuz var
+// 6 = 1, 2, 3, 6 = 12 = 6*2
+
+// 28 = 1, 2, 4, 7, 14, 28 = 56 = 28*2
+
+// isPerfectNumber(42);
+// isPerfectNumber(8128);
+
+// function isPerfectNumber(number){
+//     let toplam =0;
+//     for(let i = 2 ; i<=number/2 ; i++){
+//         if(number%i==0){
+//             toplam+=i;
+//         }
+//     }
+//     toplam+=1+number;
+//     if(toplam==number*2){
+//         console.log("Mükemmel sayıdır..");
+//     }else{
+//         console.log("Mükemmel sayı değildir!!");
+//     }
 // }
