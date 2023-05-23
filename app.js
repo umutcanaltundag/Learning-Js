4de6757f1d5b5bffd8ac77bac974cf2f1ee1522e
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-23 07:35:00 +0300

    Ürün Arama Uygulaması | Diziler

--- app.js
+++ app.js
@@ -1214,7 +1214,7 @@ switch (secim){
 //     }
 // }
 
-*/
+
 
 //!----DİZİLER-----
 
@@ -1265,7 +1265,7 @@ switch (secim){
 // });
 
 
-//!--------- Dizinin Metotları-------
+//!--------- DİZİ METOTLARI !!-------
 
 //   push    : dizinin sonuna eleman ekler , ayrıca dizinin uzunluğunu döner
 //   unshift : dizinin başına eleman ekler , eleman sayısını geriye döner
@@ -1387,3 +1387,56 @@ let arabalar =  ["Bmw","Audi","Mercedes","Porsche","Supra"];
 // }else{
 //     console.log("Eleman yoktur!");
 // }
+
+*/
+
+
+let urun1 = {
+    isim: "ACER Swift",
+    kategori: "Teknoloji",
+    fiyat: 6.219
+}
+let urun2 = {
+    isim: "ACER Nitro 5",
+    kategori: "Teknoloji",
+    fiyat: 15.475
+}
+let urun3 = {
+    isim: "LENOVO V15",
+    kategori: "Teknoloji",
+    fiyat: 10.999
+}
+let urun4 = {
+    isim: "LENOVO V14",
+    kategori: "Teknoloji",
+    fiyat: 4.399
+}
+let urun5 = {
+    isim: "LENOVO Idepad",
+    kategori: "Teknoloji",
+    fiyat: 4.510
+}
+let urunler = [urun1, urun2, urun3, urun4, urun5];
+let filtreliUrunler = [];
+let kullaniciUrunIsmi = prompt("Bir ürün ismi giriniz");
+
+filtreliUrunleriDoldur(urunler);
+filtreliUrunleriYazdir(filtreliUrunler);
+
+
+function filtreliUrunleriDoldur(urunler) {
+    urunler.forEach(function (urun) {
+        if (urun.isim.toUpperCase().includes(kullaniciUrunIsmi.toUpperCase(), 0)) {
+            filtreliUrunler.push(urun);
+        }
+    });
+
+}
+
+function filtreliUrunleriYazdir(urunler) {
+    urunler.forEach(function (urun) {
+        console.log("---------------------------------------");
+        console.log("|" + urun.isim + "|" + urun.fiyat + "|" + urun.kategori);
+        console.log("---------------------------------------");
+    });
+}
