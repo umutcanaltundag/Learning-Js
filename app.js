315e57b0ccfe75b23bf819afffe4514c7ccc77b5
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-24 07:15:46 +0300

    Kitap Bulma Uygulaması | Diziler

--- app.js
+++ app.js
@@ -1388,55 +1388,153 @@ let arabalar =  ["Bmw","Audi","Mercedes","Porsche","Supra"];
 //     console.log("Eleman yoktur!");
 // }
 
+
+
+
+// let urun1 = {
+//     isim: "ACER Swift",
+//     kategori: "Teknoloji",
+//     fiyat: 6.219
+// }
+// let urun2 = {
+//     isim: "ACER Nitro 5",
+//     kategori: "Teknoloji",
+//     fiyat: 15.475
+// }
+// let urun3 = {
+//     isim: "LENOVO V15",
+//     kategori: "Teknoloji",
+//     fiyat: 10.999
+// }
+// let urun4 = {
+//     isim: "LENOVO V14",
+//     kategori: "Teknoloji",
+//     fiyat: 4.399
+// }
+// let urun5 = {
+//     isim: "LENOVO Idepad",
+//     kategori: "Teknoloji",
+//     fiyat: 4.510
+// }
+// let urunler = [urun1, urun2, urun3, urun4, urun5];
+// let filtreliUrunler = [];
+// let kullaniciUrunIsmi = prompt("Bir ürün ismi giriniz");
+
+// filtreliUrunleriDoldur(urunler);
+// filtreliUrunleriYazdir(filtreliUrunler);
+
+
+// function filtreliUrunleriDoldur(urunler) {
+//     urunler.forEach(function (urun) {
+//         if (urun.isim.toUpperCase().includes(kullaniciUrunIsmi.toUpperCase(), 0)) {
+//             filtreliUrunler.push(urun);
+//         }
+//     });
+
+// }
+
+// function filtreliUrunleriYazdir(urunler) {
+//     urunler.forEach(function (urun) {
+//         console.log("---------------------------------------");
+//         console.log("|" + urun.isim + "|" + urun.fiyat + "|" + urun.kategori);
+//         console.log("---------------------------------------");
+//     });
+// }
+
 */
 
 
-let urun1 = {
-    isim: "ACER Swift",
-    kategori: "Teknoloji",
-    fiyat: 6.219
-}
-let urun2 = {
-    isim: "ACER Nitro 5",
-    kategori: "Teknoloji",
-    fiyat: 15.475
-}
-let urun3 = {
-    isim: "LENOVO V15",
-    kategori: "Teknoloji",
-    fiyat: 10.999
-}
-let urun4 = {
-    isim: "LENOVO V14",
-    kategori: "Teknoloji",
-    fiyat: 4.399
-}
-let urun5 = {
-    isim: "LENOVO Idepad",
-    kategori: "Teknoloji",
-    fiyat: 4.510
-}
-let urunler = [urun1, urun2, urun3, urun4, urun5];
-let filtreliUrunler = [];
-let kullaniciUrunIsmi = prompt("Bir ürün ismi giriniz");
+let kitap1 = { isim: "Her Şeyi Düşünme", yazar: "Anne Bogel", fiyat: 25, raf: "1.5.RAF" }
+let kitap2 = { isim: "Hiçbir Karşılaşma Tesadüf Değildir", yazar: "Hakan Mengüç", fiyat: 56, raf: "2.3.RAF" }
+let kitap3 = { isim: "İnsan Neyle Yaşar", yazar: "Tolstoy", fiyat: 34, raf: "3.4.RAF" }
+let kitap4 = { isim: "Zafer Sızlanarak Kazanılmaz", yazar: "Haluk Tatar", fiyat: 45, raf: "4.1.RAF" }
+let kitap5 = { isim: "Şeker Portakalı", yazar: "José Mauro de Vasconcelos", fiyat: 22, raf: "5.3.RAF" }
 
-filtreliUrunleriDoldur(urunler);
-filtreliUrunleriYazdir(filtreliUrunler);
+let kitaplar = [kitap1, kitap2, kitap3, kitap4, kitap5];
 
 
-function filtreliUrunleriDoldur(urunler) {
-    urunler.forEach(function (urun) {
-        if (urun.isim.toUpperCase().includes(kullaniciUrunIsmi.toUpperCase(), 0)) {
-            filtreliUrunler.push(urun);
+let raf11 = { kod: "1.1RAF", goster: false }
+let raf12 = { kod: "1.2RAF", goster: false }
+let raf13 = { kod: "1.3RAF", goster: false }
+let raf14 = { kod: "1.4RAF", goster: false }
+let raf15 = { kod: "1.5RAF", goster: false }
+
+
+let raf21 = { kod: "2.1.RAF", goster: false }
+let raf22 = { kod: "2.2.RAF", goster: false }
+let raf23 = { kod: "2.3.RAF", goster: false }
+let raf24 = { kod: "2.4.RAF", goster: false }
+let raf25 = { kod: "2.5.RAF", goster: false }
+
+
+let raf31 = { kod: "3.1.RAF", goster: false }
+let raf32 = { kod: "3.2.RAF", goster: false }
+let raf33 = { kod: "3.3.RAF", goster: false }
+let raf34 = { kod: "3.4.RAF", goster: false }
+let raf35 = { kod: "3.5.RAF", goster: false }
+
+
+let raf41 = { kod: "4.1.RAF", goster: false }
+let raf42 = { kod: "4.2.RAF", goster: false }
+let raf43 = { kod: "4.3.RAF", goster: false }
+let raf44 = { kod: "4.4.RAF", goster: false }
+let raf45 = { kod: "4.5.RAF", goster: false }
+
+
+let raf51 = { kod: "5.1.RAF", goster: false }
+let raf52 = { kod: "5.2.RAF", goster: false }
+let raf53 = { kod: "5.3.RAF", goster: false }
+let raf54 = { kod: "5.4.RAF", goster: false }
+let raf55 = { kod: "5.5.RAF", goster: false }
+
+let raflar = [
+    [raf51, raf52, raf53, raf54, raf55],
+    [raf41, raf42, raf43, raf44, raf45],
+    [raf31, raf32, raf33, raf34, raf35],
+    [raf21, raf22, raf23, raf24, raf25],
+    [raf11, raf12, raf13, raf14, raf15]];
+
+function rafOlustur() {
+    console.clear();
+    let satir = "";
+    for (let i = 0; i < raflar.length; i++) {
+        for (let j = 0; j < 5; j++) {
+            satir += "|" + (raflar[i][j].goster ? raflar[i][j].kod : "---") + "";
+        }
+        console.log(satir);
+        console.log("--------------------");
+        satir = "";
+    }
+}
+
+function kodBul(kitapIsmi) {
+    let rafKod=null;
+    kitaplar.forEach(function (kitap) {
+        if (kitap.isim.toUpperCase().includes(kitapIsmi.toUpperCase(), 0)) {
+            rafKod=kitap.raf;
         }
     });
-
+        return rafKod;
 }
 
-function filtreliUrunleriYazdir(urunler) {
-    urunler.forEach(function (urun) {
-        console.log("---------------------------------------");
-        console.log("|" + urun.isim + "|" + urun.fiyat + "|" + urun.kategori);
-        console.log("---------------------------------------");
-    });
+function raftaGoster(rafKodu) {
+    for (let i = 0; i < raflar.length; i++) {
+        for (let j = 0; j < 5; j++) {
+            if (raflar[i][j].kod == rafKodu) {
+                raflar[i][j].goster = true;
+                break;
+            }
+        }
+    }
+}
+rafOlustur();
+
+let kitapIsmi = prompt("Lütfen bir kitap ismi giriniz.");
+let rafKod = kodBul(kitapIsmi);
+console.log(rafKod);
+if (rafKod != null) {
+    raftaGoster(rafKod);
+    rafOlustur();
+} else {
+    alert("Girdiğiniz kitap kütüphanemizde bulunmamaktadır :( ")
 }
