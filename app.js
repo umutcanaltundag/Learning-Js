4679f246c0dca51000ffe2e1435e1dd822c8b246
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-05-28 16:12:28 +0300

    İnput Events

--- app.js
+++ app.js
@@ -2023,7 +2023,7 @@ let tarih ="2023";
 //     console.log(e.type);
 // }
 
-*/
+
 
 
 //! Klavye Eventları 
@@ -2052,4 +2052,32 @@ let tarih ="2023";
 
 // function run(e){
 //     cardTitle.textContent = e.target.value;
+// }
+
+
+*/
+
+
+//! İnput Events
+
+//focus 
+//blur
+//copy
+//paste
+//cut
+//select
+
+
+// const todo = document.querySelector("#todoName");
+
+// todo.addEventListener("focus", run);
+// todo.addEventListener("blur",run);
+// todo.addEventListener("copy",run);
+// todo.addEventListener("paste",run);
+// todo.addEventListener("cut",run);
+// todo.addEventListener("select", run);
+
+
+// function run(e){
+//     console.log(e.type);
 // }
