cc0a44f06d8ac1338d98023d2647f4c655af4469
Umutcan ALTUNDAG <statikebedi63@gmail.com>
2023-06-05 16:19:31 +0300

    PROMİSE + XMLHTTPREQUEST

--- app.js
+++ app.js
@@ -2899,7 +2899,7 @@ let userId = getUserId();
 getPostByUserId(userId);
 
 
-*/
+
 
 // ! AJAX NEDİR? 
 
@@ -2936,3 +2936,150 @@ getPostByUserId(userId);
 //     xhr.send();
 // }
 // getData("https://jsonplaceholder.typicode.com/posts")
+
+
+*/
+
+
+//?       P R O M İ S E
+
+// let check = false;
+
+// const promise1 = new Promise((resolve , reject)=>{
+//     if(check){
+//         resolve("Promise Başarılı")
+//     }else{
+//         reject("Promise başarısız..");
+//     }
+// })
+
+// console.log(promise1);
+
+
+// asenkron yapıları senkrona çevirmek için kullanıyoruz.
+//CALLBACK lerin alternatifi
+//ES6
+
+// let check = true;
+// function createPromise(){
+//     return new Promise((resolve, reject)=>{
+//         if(check){
+//             resolve("Promise'te herhangi bir sıkıntı yok")
+//         }else{
+//             reject("Sıkıntı büyük :D")
+//         }
+//     })
+// }
+
+// createPromise()
+// .then((response)=>{
+//     console.log(response);
+// })
+
+// .catch((error)=>{
+//     console.log(error);
+// })
+
+// .finally(()=> console.log("Her zaman çalışır"))
+
+
+
+
+//?  PROMİSE + XMLHTTPREQUEST
+
+//Promise.all
+
+function readStudents(url) {
+
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        try {
+            xhr.addEventListener("readystatechange", () => {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                }
+            })
+        } catch (error) {
+            reject(error);
+        }
+
+        xhr.open("GET", url);
+        xhr.send();
+    })
+}
+
+
+// readStudents("student.json")
+// .then((data)=> console.log(data))
+// .catch((err)=>console.log(err))
+
+
+
+function getUsers(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", () => {
+            try {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText))
+                }
+            } catch (error) {
+                reject(error);
+            }
+        })
+        xhr.open("GET", url);
+        xhr.send();
+    })
+}
+
+
+function getCommentsByUserID(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", () => {
+            try {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText))
+                }
+            } catch (error) {
+                reject(error);
+            }
+        })
+        xhr.open("GET", url);
+        xhr.send();
+    })
+}
+
+
+
+// getUsers("https://jsonplaceholder.typicode.com/users/3")
+//     .then((data) => {
+//         console.log(data);
+//         return getCommentsByUserID(`https://jsonplaceholder.typicode.com/comments/${data.id}`)
+//     })
+
+//     .then((res)=>console.log(res))
+//     .catch((err) => console.log(err))
+//     .finally(() => {
+//         // mail atma kodlarını yazarız.
+//     });  
+
+
+
+// const p1 = Promise.resolve("birinci promise başarılı")
+// const p2 = Promise.resolve("ikinci promise başarılı")
+// const p3 = new Promise((resolve, reject) => {
+//     resolve("üçüncü promise başarılı")
+// })
+
+// const p4 = Promise.reject("Hata var umut reyiz.");
+
+// Promise.all([p1, p2, p3, p4])
+//     .then((res) => {
+//         console.log(res);
+//         // for (let value of res) {
+//         //     console.log(value);
+//         // }
+//     })
+//     .catch((err) => console.log(err))
+
